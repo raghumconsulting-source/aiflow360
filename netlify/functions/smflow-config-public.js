142194ad099b95629ba4b12d441c4466a04c5b48
Raghu <raghum.consulting@gmail.com>
2026-06-05 20:35:03 +1000

    fix: smflow-config-public reads from env vars only

---
+++ netlify/functions/smflow-config-public.js
@@ -0,0 +1,13 @@
+﻿exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      supabase_url: process.env.SUPABASE_URL,
+      supabase_anon: process.env.SUPABASE_ANON_KEY
+    })
+  };
+};
